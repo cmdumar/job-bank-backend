ce2413baaf4234786fa0c2711f7a978542037760
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-15 18:01:56 +0530

    Add Profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/jobs/:id', to: 'job#show'
+  get '/profiles/:username', to: 'profile#show'
 end
