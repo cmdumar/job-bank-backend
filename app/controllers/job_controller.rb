97829652077339d088cf98baf5e9a28abc5498ce
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-15 18:14:10 +0530

    Both endpoints working

--- app/controllers/job_controller.rb
+++
@@ -1,6 +0,0 @@
-class JobController < ApplicationController
-  def show
-    job = RestClient.get("https://torre.co/api/opportunities/#{params[:id]}")
-    render json: job
-  end
-end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,6 @@
 class ProfileController < ApplicationController
   def show
     profile = RestClient.get("https://torre.bio/api/bios/#{params[:username]}")
-    render json: person
+    render json: profile
   end
 end
