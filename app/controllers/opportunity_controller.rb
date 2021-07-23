c8c18bc6db582a225bfd438bd311141d0c578f9b
Muhammad Umar <mohammadumar28@gmail.com>
2021-07-23 16:45:29 +0530

    Filter the unnecessary data from job api

--- app/controllers/opportunity_controller.rb
+++ app/controllers/opportunity_controller.rb
@@ -1,6 +1,16 @@
 class OpportunityController < ApplicationController
   def show
     opportunity = RestClient.get("https://torre.co/api/opportunities/#{params[:id]}")
-    render json: opportunity
+    parsed = JSON.parse(opportunity)
+    js = Hash.new
+    js[:objective] = parsed["objective"]
+    js[:organizations] = parsed["organizations"]
+    js[:place] = parsed["place"]
+    js[:compensation] = parsed["compensation"]
+    js[:languages] = parsed["languages"]
+    js[:strengths] = parsed["strengths"]
+    js[:details] = parsed["details"]
+    js[:members] = parsed["members"]
+    render json: js
   end
 end
